fa734e9ce28249da1cd5b1006452ed660bdb2e5a
liyt119 <891759142@qq.com>
2021-03-19 10:39:03 +0800

    注册登录修复

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,37 +9,37 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{route('map')}}">地图 <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{route('upload')}}">导入文件</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('usersshow')}}">用户信息</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0" method="post" action="{{route('res.search')}}">
-    @csrf
-      <input class="form-control mr-sm-2" type="search" name="address" id="address" value="@isset($address_raw){{ $address_raw }}@endisset" placeholder="请输入用户姓名或邮箱" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{route('map')}}">地图 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('upload')}}">导入文件</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('usersshow')}}">用户信息</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0" method="post" action="{{route('res.search')}}">
+        @csrf
+        <input class="form-control mr-sm-2" type="search" name="address" id="address" value="@isset($address_raw){{ $address_raw }}@endisset" placeholder="请输入用户姓名或邮箱" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
@@ -50,8 +50,28 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        @guest
         <li class="nav-item"><a class="nav-link" href="{{route('login')}}">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="{{route('register')}}">注册</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="{{Auth::user()->avatar}}" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="">个人中心</a>
+            <a class="dropdown-item" href="">编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" id="logout" href="#">
+              <form action="{{ route('logout') }}" method="POST">
+                {{ csrf_field() }}
+                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+              </form>
+            </a>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
