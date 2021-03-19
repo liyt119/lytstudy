8c0daf86a929355bc38f3ae2a543659001bf6fb0
liyt119 <891759142@qq.com>
2021-03-19 09:57:11 +0800

    生成用户认证代码

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,27 @@ Route::get('/usershow', 'UsersController@show')->name('usersshow');
 Route::get('/res', 'UsersController@root')->name('res.root');
 Route::get('/res/search', 'UsersController@root');
 Route::post('/res/search', 'UsersController@search')->name('res.search');
+
+//Auth::routes();
+// 用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// 密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+//Route::get('/home', 'HomeController@index')->name('home');
